098cab96b8cf534e8d79f139d0345289d20939dc
Andrya_A354 <andrya.afgani@gmail.com>
2020-08-21 09:59:15 +0700

    init database project

---
+++ WebClient.Database/dbo/Tables/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Fullname]   NVARCHAR (MAX) NULL,
+    [Department] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
