3eda602a0b9049aa6b549f8375255e5bf1aedc4b
Andrya_A354 <andrya.afgani@gmail.com>
2020-08-21 09:51:56 +0700

    add web client

---
+++ WebClient/Data/WebClientContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebClient.Models;
+
+namespace WebClient.Data
+{
+    public class WebClientContext : DbContext
+    {
+        public WebClientContext (DbContextOptions<WebClientContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<WebClient.Models.Employee> Employee { get; set; }
+    }
+}
